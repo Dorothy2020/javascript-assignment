7d8cbd9e22a0e7270ac7b84b3384e2bbdc9c2294
Dorothy2020 <wangarraakoth@gmail.com>
2021-03-08 17:03:18 +0300

    assignment1

---
+++ script.js
@@ -0,0 +1,8 @@
+function fruit(){
+    var fruits=["Strawberry", "Kiwi", "Guava","Mangoes","Tomatoes", "Grapes"];
+    fruits.forEach(element => console.log (element))
+
+  }
+  fruit();
+
+        
